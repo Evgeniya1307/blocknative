bad53a68f5077f3f64b15e528be39408f719265b
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-10-31 12:45:38 +0400

    Navigation=()=>{//этот компонент получает спец разметку

---
+++ components/Loading.jsx
@@ -0,0 +1,18 @@
+import { ActivityIndicator, Text, View } from "react-native";
+
+export const Loading = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ActivityIndicator size="large" />
+      <Text style={{ marginTop: 15 }}>Загрузка...</Text>
+    </View>
+  );
+};
+
+export default Loading;
